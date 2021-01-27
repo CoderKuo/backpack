2fb15fe60877f2a7b0a50966972613b595c22ec7
dkinging <1520622465@qq.com>
2021-01-27 21:49:19 +0800

    [+]背包YAML配置实体类

---
+++ src/main/java/com/dakuo/backpack/entity/BackPackYamlEntity.java
@@ -0,0 +1,62 @@
+package com.dakuo.backpack.entity;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class BackPackYamlEntity {
+    String name;
+    String description;
+    int size;
+    JSONObject level;
+
+    public BackPackYamlEntity() {
+    }
+
+    public BackPackYamlEntity(String name, String description, int size, JSONObject level) {
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public JSONObject getLevel() {
+        return level;
+    }
+
+    public void setLevel(JSONObject level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "BackPackYamlEntity{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", size=" + size +
+                ", level=" + level +
+                '}';
+    }
+}
